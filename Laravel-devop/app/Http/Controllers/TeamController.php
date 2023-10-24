5b39e74b1436e4f028eda9f86d4ab7650dacb11d
Pamiralhp <132684622+Pamiralhp@users.noreply.github.com>
2023-10-24 12:15:13 +0200

    creation of team controller

--- Laravel-devop/app/Http/Controllers/TeamController.php
+++ Laravel-devop/app/Http/Controllers/TeamController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Team;
 
 class TeamController extends Controller
 {
-    //
+    public function index()
+    {
+        $teams = Team::all(); 
+        return view('team.index')->with('teams', $teams);
+    }
+
+    public function create()
+    {   
+        
+        return view('team.create'); 
+    }
 }
