db9aaf171c5c58fc6668b591ead11fd71da20f54
Pamiralhp <132684622+Pamiralhp@users.noreply.github.com>
2023-11-07 15:51:02 +0100

    Hot/fix-CreateGames

--- routes/web.php
+++ routes/web.php
@@ -30,18 +30,21 @@ Route::middleware('auth')->group(function () {
 });
 
 // Routes for Teams
+// Teams routes
 Route::get('/teams', [TeamController::class, 'index'])->name('teams.index');
 Route::get('/teams/create', [TeamController::class, 'create'])->name('teams.create');
 Route::post('/teams', [TeamController::class, 'store'])->name('teams.store');
+Route::get('/teams/{id}/edit', [TeamController::class, 'edit'])->name('teams.edit');
+Route::put('/teams/{id}', [TeamController::class, 'update'])->name('teams.update');
 Route::delete('/teams/{id}', [TeamController::class, 'destroy'])->name('teams.destroy');
-Route::get('/teams/{team}/edit', [TeamController::class, 'edit'])->name('teams.edit');
-Route::get('/teams/{team}', [TeamController::class, 'update'])->name('teams.update');
 
 // Routes for Games
-Route::get('/games/{game}', [GameController::class, 'update'])->name('games.update');
-Route::get('/games/{game}/edit', [GameController::class, 'edit'])->name('games.edit');
 Route::get('/games/create', [GameController::class, 'create'])->name('games.create');
 Route::post('/games', [GameController::class, 'store'])->name('games.store');
 Route::get('/games', [GameController::class, 'index'])->name('games.index');
+Route::get('/games/{game}', [GameController::class, 'show'])->name('games.show');
+Route::get('/games/{game}/edit', [GameController::class, 'edit'])->name('games.edit');
+Route::patch('/games/{game}', [GameController::class, 'update'])->name('games.update');
 Route::delete('/games/{game}', [GameController::class, 'destroy'])->name('games.destroy');
+
 require __DIR__.'/auth.php';
